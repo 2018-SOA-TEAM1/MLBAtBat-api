997f5f57edecdce02e0d074dd542ed321bf8c448
sam <s5078345@gmail.com>
2018-12-31 15:34:01 +0800

    Add messaging. (Not done yet).

---
+++ app/infrastructure/messaging/queue.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module MLBAtBat
+    module Messaging
+      ## Queue wrapper for AWS SQS
+      # Requires: AWS credentials loaded in ENV or through config file
+      class Queue
+        IDLE_TIMEOUT = 5 # seconds
+  
+      end
+    end
+  end
+  
